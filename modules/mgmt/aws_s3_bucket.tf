5eeaa14cc6d0fbe289319d565df338d94f6967e5
tunarider <tunarider@tunaground.net>
2021-07-08 16:53:52 +0900

    mgmt s3 저장소 이름 규칙 설정

--- modules/mgmt/aws_s3_bucket.tf
+++ modules/mgmt/aws_s3_bucket.tf
@@ -1,9 +1,15 @@
-resource "random_id" "s3_mgmt_id" {
-  byte_length = 8
+resource "random_string" "s3_mgmt_id" {
+  length      = 16
+  lower       = true
+  upper       = false
+  number      = true
+  special     = false
+  min_lower   = 1
+  min_numeric = 1
 }
 
 resource "aws_s3_bucket" "mgmt" {
-  bucket = "${lower(var.project)}-mgmt-${lower(random_id.s3_mgmt_id.id)}"
+  bucket = "${lower(var.project)}-mgmt-${random_string.s3_mgmt_id.result}"
   acl    = "private"
 
   tags = merge(
