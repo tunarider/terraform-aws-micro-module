b09854658f4abad68eb64b65d19d184d3cac04b8
tunarider <tunarider@tunaground.net>
2021-07-15 12:41:05 +0900

    base output 출력

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,27 @@
 output "bastion_ip" {
   value = module.bastion.bastion_ip
 }
+
+output "aws_vpc_id" {
+  value = module.base.aws_vpc_id
+}
+
+output "aws_default_security_group_default_id" {
+  value = module.base.aws_default_security_group_default_id
+}
+
+output "aws_public_subnet_ids" {
+  value = module.base.aws_public_subnet_ids
+}
+
+output "aws_private_was_subnet_ids" {
+  value = module.base.aws_private_was_subnet_ids
+}
+
+output "aws_private_db_subnet_ids" {
+  value = module.base.aws_private_db_subnet_ids
+}
+
+output "aws_route53_private_id" {
+  value = module.base.aws_route53_private_id
+}
