3c2cb68939d95bfd8acf32b96f6c9283a3680a26
tunarider <tunarider@tunaground.net>
2021-07-23 19:55:32 +0900

    버전 형식 변경

--- modules/mgmt/main.tf
+++ modules/mgmt/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.0.0, < 1.1.0"
+  required_version = "~> 1.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.42.0, < 3.43.0"
+      version = "~> 3.42"
     }
 
     random = {
